0e1d310ddf7ec51f7e31caaa53837ff635126219
cpowicki <cpowicki@tulane.edu>
2017-12-06 03:46:24 +0000

    Fix minor display issues on show pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205064958) do
+ActiveRecord::Schema.define(version: 20171206025625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,26 +29,14 @@ ActiveRecord::Schema.define(version: 20171205064958) do
   add_index "joinrequests", ["property_id"], name: "index_joinrequests_on_property_id", using: :btree
   add_index "joinrequests", ["tenant_id"], name: "index_joinrequests_on_tenant_id", using: :btree
 
-  create_table "landlords", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "office_address"
-    t.string   "phone_number"
-    t.string   "email"
-    t.text     "notes"
-    t.string   "password"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
   create_table "managers", force: :cascade do |t|
     t.string   "name"
     t.string   "provider"
     t.string   "uid"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
     t.string   "email"
     t.string   "password"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.string   "phone_number"
   end
 
@@ -60,21 +48,25 @@ ActiveRecord::Schema.define(version: 20171205064958) do
     t.integer  "curr_num_tenants"
     t.decimal  "monthly_rent"
     t.text     "notes"
+    t.integer  "manager_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
-    t.integer  "manager_id"
     t.integer  "rent"
   end
 
+  add_index "properties", ["manager_id"], name: "index_properties_on_manager_id", using: :btree
+
   create_table "tenants", force: :cascade do |t|
+    t.string   "name"
+    t.string   "phone_number"
+    t.string   "email"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "password"
+    t.integer  "property_id"
+    t.integer  "true_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.string   "name"
-    t.string   "email"
-    t.string   "password"
-    t.string   "phone_number"
-    t.integer  "property_id"
-    t.string   "rent"
   end
 
 end
