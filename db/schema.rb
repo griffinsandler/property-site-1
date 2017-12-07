ef59d954c55139b9d780a8e51296360795c4c6e0
Michael Redston <redston96@yahoo.com>
2017-12-07 00:28:30 +0000

    Service request now uses users information

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,21 @@ ActiveRecord::Schema.define(version: 20171206040540) do
   add_index "joinrequests", ["property_id"], name: "index_joinrequests_on_property_id", using: :btree
   add_index "joinrequests", ["tenant_id"], name: "index_joinrequests_on_tenant_id", using: :btree
 
+  create_table "landlords", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "office_address"
+    t.string   "phone_number"
+    t.string   "email"
+    t.text     "notes"
+    t.string   "password"
+    t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
   create_table "managers", force: :cascade do |t|
     t.string   "name"
     t.string   "provider"
@@ -79,10 +94,9 @@ ActiveRecord::Schema.define(version: 20171206040540) do
     t.string   "provider"
     t.string   "uid"
     t.string   "password"
-    t.integer  "property_id"
-    t.integer  "true_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "property_id"
     t.integer  "rent"
   end
 
